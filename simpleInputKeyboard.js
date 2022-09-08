d25e975a03fd50e05d8dce6a0d81ea771780c88d
pkindalov <panayotkindalov@gmail.com>
2022-09-08 11:53:21 +0300

    Added more inputs for the  testing purposes. Added some todos for later and fixed some small errors. Still work in progress.. need refactoring

--- simpleInputKeyboard.js
+++ simpleInputKeyboard.js
@@ -3,10 +3,12 @@ const simpleInputKeyboard = (function () {
   let config;
   let className;
   let inputs;
+  let inputNum;
 
   function initializeConfiguration(settings) {
     className = 'bg-keyboard';
     inputs = [];
+    inputNum = 0;
   }
 
   function getInputs() {
@@ -15,14 +17,52 @@ const simpleInputKeyboard = (function () {
 
   function addEventListeners(inputs) {
     Array.from(inputs).forEach(input => {
-      if (!input.hasListener) {
+      if (!input.hasFocusListener) {
         input.addEventListener('focus', (event) => showKeyBoard(event));
+        input.hasFocusListener = true;
       }
     });
   }
 
   function showKeyBoard(event) {
-    console.log(event);
+    if (!event || !event.target) {
+      console.log('Problem with event');
+      return;
+    }
+    const currentInput = event.target;
+    const currentInputCoords = currentInput.getBoundingClientRect();
+    const currentInputWidth = currentInput.offsetWidth;
+    const currentInputParent = event.target.parentNode;
+
+    //TODO if there is appended div container with the keyboard
+    console.log(currentInputCoords);
+    // console.log(currentInputParent.getAttribute('data-hasКeyboard'));
+    // console.log(currentInputParent.dataset);
+    // console.log(currentInputParent.dataset.hasКeyboard);
+    // console.log(currentInput?.nextSibling?.previousSibling?.classList.contains(className));
+    //TODO to create a function which will return div container with the keyboard and to append it after the current input
+
+    // if (document.getElementsByClassName(genClass).length) return;
+    if (currentInput.dataset.hasКeyboard) return;
+    inputNum++;
+    let test = document.createElement('div');
+    let genClass = 'bg-keyboard-' + inputNum;
+    test.innerText = 'test' + inputNum;
+    test.setAttribute('class', genClass);
+    test.style.backgroundColor = 'green';
+    test.style.borderRadius = '5px';
+    test.style.width = ((currentInputWidth / 10) - 6.5) + 'rem';
+    test.style.position = 'absolute';
+    test.style.top = ((currentInputCoords.top + 12) / 10) + 'rem';
+    // test.style.left = ((currentInputCoords.left / 10) - 0.3) + 'rem';
+    test.style.left = currentInputCoords.left + 'px';
+    test.style.zIndex = '99999999999999999999999999';
+    currentInput.setAttribute('data-hasКeyboard', true);
+    document.body.appendChild(test);
+
+    //TODO to put blur listener, to delete keyboard window
+
+    // currentInputParent.insertBefore(test, currentInput.nextSibling);
   }
 
   // we export the centralized method for retrieving the singleton value
